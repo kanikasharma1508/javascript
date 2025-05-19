0ad9996caed232f5e8169aa27f8821002628d36e
kanikasharma1508 <kanikasharma9006@gmail.com>
2025-05-20 00:14:47 +0530

    Add files via upload

---
+++ loopQ3.js
@@ -0,0 +1,17 @@
+let a=10
+let isprime=1
+for(i=2;i<a;i++)
+{
+    if(a%i==0)
+    { isprime=0
+
+    }
+
+}
+if(isprime==1)
+{
+    console.log("prime")
+}
+else{
+    console.log("not a prime")
+}
---
+++ whileQ1.js
@@ -0,0 +1,6 @@
+let a=1
+while(a<=100)
+{
+    console.log(a)
+    a++
+}
